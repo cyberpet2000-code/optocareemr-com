db9c66ed6b8eff2c7f14440173b684a950842762
cyberpet2000-code <cyberpet2000@gmail.com>
2026-05-21 17:08:59 +0100

    Update Appointments.tsx

--- src/pages/Appointments.tsx
+++ src/pages/Appointments.tsx
@@ -56,7 +56,6 @@ console.log("appointments data", data);
   data?.map((a: any) => a.clinic_id)
 );
      
-);
     console.debug("[appointments]", { clinic_id: cid, count: data?.length ?? 0 });
     if (data && data.length > 0) {
       const patientIds = [...new Set(data.filter((a: any) => a.patient_id).map((a: any) => a.patient_id))];
