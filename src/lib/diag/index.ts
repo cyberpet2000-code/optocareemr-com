014a5158c61f0393821455daac79d1a24e21c68d
cyberpet2000-code <cyberpet2000@gmail.com>
2026-06-07 06:05:37 +0100

    Update index.ts

--- src/lib/diag/index.ts
+++ src/lib/diag/index.ts
@@ -3,3 +3,7 @@ export { isDiagEnabled, setDiagEnabled } from "./diagConfig";
 export { installDiagFetchPatch } from "./diagFetchPatch";
 export { installRuntimeErrorDetector } from "./runtimeErrorDetector";
 export { default as DiagOverlay } from "./DiagOverlay";
+export {
+  startLoadingWatch,
+  stopLoadingWatch,
+} from "./loadingDetector";
