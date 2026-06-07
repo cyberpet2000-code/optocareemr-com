344d284fa9017f3bc627506e5391571409db349f
cyberpet2000-code <cyberpet2000@gmail.com>
2026-06-07 10:21:21 +0100

    Update executiveSummary.ts

--- src/lib/diag/executiveSummary.ts
+++ src/lib/diag/executiveSummary.ts
@@ -1,1 +1,30 @@
+import { getIssues } from "./issueTracker";
+import { analyzePriority } from "./priorityAnalyzer";
 
+export function getExecutiveSummary() {
+  const issues = getIssues();
+
+  const openIssues = issues.filter(
+    (i) => i.status === "open"
+  );
+
+  const highest =
+    openIssues
+      .map((i) => ({
+        issue: i,
+        priority: analyzePriority(i.name),
+      }))
+      .sort(
+        (a, b) =>
+          b.priority.score -
+          a.priority.score
+      )[0];
+
+  return {
+    openCount: openIssues.length,
+    resolvedCount:
+      issues.length - openIssues.length,
+    highestPriority:
+      highest?.issue?.name ?? "None",
+  };
+}
