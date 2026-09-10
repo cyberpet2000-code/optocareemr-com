a93e3063be68b199e7a01c47fb7409f21bceea7a
cyberpet2000-code <cyberpet2000@gmail.com>
2026-09-10 15:44:11 +0100

    Update PatientRegister.tsx

--- src/pages/PatientRegister.tsx
+++ src/pages/PatientRegister.tsx
@@ -386,7 +386,6 @@ export default function PatientRegister() {
       phone: normalizePhone(form.phone, form.phoneCountryCode),
       p_age: parseInt(form.age, 10),
       p_gender: form.gender,
-      phone: normalizePhone(form.phone, form.phoneCountryCode),
     });
     setLoading(false);
 
