cd17480aeb65a986e426104fa91b30490d3a8724
cyberpet2000-code <cyberpet2000@gmail.com>
2026-09-18 01:16:05 +0100

    Fix DOB age helper syntax

--- src/pages/PatientRegister.tsx
+++ src/pages/PatientRegister.tsx
@@ -57,9 +57,7 @@ function escapeLikeTerm(value: string) {
 }
 
 function escapeRegExp(value: string) {
-  return value.replace(/[.*+?^${}()|[\]\\]/g, "\\function escapeRegExp(value: string) {
-  return value.replace(/[.*+?^${}()|[\]\\]/g, "\\$&");
-}");
+  return value.replace(/[.*+?^\${}()|[\]\\]/g, "\\$&");
 }
 
 function calculateAgeFromDob(value: string) {
