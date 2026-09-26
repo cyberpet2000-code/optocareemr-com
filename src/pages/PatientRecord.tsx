a0f41590fdf21b37c41d31530b6934fce99c479f
cyberpet2000-code <cyberpet2000@gmail.com>
2026-09-26 09:07:48 +0100

    feat: use shared failure diagnosis in core screens

--- src/pages/PatientRecord.tsx
+++ src/pages/PatientRecord.tsx
@@ -10,6 +10,7 @@ import { Textarea } from "@/components/ui/textarea";
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
 import { Dialog, DialogContent, DialogHeader, DialogTitle, DialogDescription, DialogFooter } from "@/components/ui/dialog";
 import { toast } from "sonner";
+import { getUserFacingErrorMessage } from "@/lib/diag/connectionDiagnosis";
 import { confirmDestructiveAction } from "@/lib/safeDelete";
 import {
   ArrowLeft,
